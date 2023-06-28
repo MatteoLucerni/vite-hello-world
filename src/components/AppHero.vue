5e80a25d894045d4e45723af04a8f9d89a079ca9
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-28 15:10:35 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            picName: 'Sekiro'
+        }
+    },
+    methods: {
+        getPath(picName) {
+            return new URL(`../assets/${picName}.webp`, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <img :src="getPath(picName)" :alt="picName">
+    </main>
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Sekiro: shadows die twice'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
